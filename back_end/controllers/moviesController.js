58ca9fc039c486ca44a248b33884243201d95ef5
Rafael Ramos <ingrnrg@gmail.com>
2023-02-14 20:37:40 -0600

    Created endpoit deleteMovie & updateMoviesr

--- back_end/controllers/moviesController.js
+++ back_end/controllers/moviesController.js
@@ -28,7 +28,35 @@ const getMovies = asyncHandler(async(req, res) => {
     res.status(200).json(movies)
 })
 
+const updateMovie = asyncHandler(async(req, res) => {
+    const Movie = await movie.findById(req.params.id)
+
+    if(!Movie){
+        res.status(400)
+        throw new Error('Movie not found')
+    }
+
+    const updateMovie = await movie.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(updateMovie)
+})
+
+const deleteMovie = asyncHandler(async (req, res) => {
+    const deleteMovie = await movie.findById(req.params.id)
+
+    if(!deleteMovie){
+        res.status(400)
+        throw new Error('Movie not found')
+    }
+
+    await deleteMovie.remove()
+
+    res.status(200).json(deleteMovie)
+})
+
 module.exports = {
     setMovie,
-    getMovies
+    getMovies,
+    updateMovie,
+    deleteMovie
 }
--- back_end/routes/moviesRoutes.js
+++ back_end/routes/moviesRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const { setMovie, getMovies } = require('../controllers/moviesController')
+const { setMovie, getMovies, updateMovie, deleteMovie } = require('../controllers/moviesController')
 
 router.route('/').post(setMovie).get(getMovies)
+router.route('/:id').put(updateMovie).delete(deleteMovie)
 
 module.exports = router
