294d54beba5d32628290b00b3d6ee7abb3b555a2
Rafael Ramos <ingrnrg@gmail.com>
2023-02-14 19:25:31 -0600

    Created errorMiddleware

--- back_end/server.js
+++ back_end/server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config()
 
 /* importaciones de funciones */
 const conectDB = require('./config/db')
+const errorHandler = require('./middleware/errorMiddleware')
 
 /* Conexion al puerto */
 const port = process.env.PORT
@@ -21,6 +22,9 @@ app.use(express.json())
 // Endpoints
 app.use('/api/actors', require('./routes/actoresRoutes'))
 app.use('/api/directors', require('./routes/directoresRoutes'))
+
+// Middleware
+app.use(errorHandler)
 
 // Conexion al puerto asignado
 app.listen(port, () => console.log(`Server started on port ${port}`))
